cbf6187ca745d062f5e1b4760abd1279332ba76e
Yash Malviya <yashmalviya98@gmail.com>
2021-03-03 16:16:14 +0530

    median cut algo correct

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import popularity
 import median_cut
 import floyd_steinberg
+# import sys
+# np.set_printoptions(threshold=sys.maxsize)
 
 import cv2 as cv
 
@@ -15,7 +17,8 @@ def showimage(imglabel, img):
 
 if __name__ == "__main__":
 	img = cv.imread("images/fox.jpg")
-	showimage("img", img)
+	# showimage("img", img)
 
 	# popularity.plot_histogram(img)
-	floyd_steinberg.dithering_algorithm(img)
+	# floyd_steinberg.dithering_algorithm(img)
+	print(median_cut.algorithm(img))
--- median_cut.py
+++ median_cut.py
@@ -1,40 +1,33 @@
 import numpy as np
 
-def medians(bucket, depth, meds=[]):
-    if bucket = []:
-        return
-    if depth == 0:
-        meds.append()
-        return meds
-    medians(bucket[],depth-1,meds)
-    medians(bucket[],depth-1,meds)
+def select_channel(bucket):
+	max_r = 255
+	max_g = 255
+	max_b = 255
+	min_r = 0
+	min_g = 0
+	min_b = 0
 
-def algorithm(img):
-    r_bucket = []
-    g_bucket = []
-    b_bucket = []
-    for x in range(img.shape[0]):
-        for y in range(img.shape[1]):
-            r_bucket.append(img[x][y][0])
-            g_bucket.append(img[x][y][1])
-            b_bucket.append(img[x][y][2])
-    
-    r_range = max(r_bucket) - min(r_bucket)
-    g_range = max(g_bucket) - min(g_bucket)
-    b_range = max(b_bucket) - min(b_bucket)
+	for r, g, b in bucket:
+		max_r = max(max_r, r)
+		max_g = max(max_g, g)
+		max_b = max(max_b, b)
+		min_r = min(min_r, r)
+		min_g = min(min_g, g)
+		min_b = min(min_b, b)
+	
+	ranges = [max_r - min_r, max_g - min_g, max_b - min_b]
+	return ranges.index(max(ranges))
 
-    bucket = [(r_bucket[i], g_bucket[i], b_bucket[i]) for i in range(len(r_bucket))]
+def median_cut(bucket, depth):
+	if depth == 0 or len(bucket) == 1:
+		return [bucket]
+	channel_for_split = select_channel(bucket)
+	to_split = sorted(bucket, key= lambda x: x[channel_for_split])
+	median_index = len(to_split)//2
+	return median_cut(bucket[:median_index], depth-1) + median_cut(bucket[median_index:], depth-1)
 
-    color_ranges = {'r': r_range, 'g': g_range, 'b': b_range}
-    chosen_color = max(color_ranges, key=color_ranges. get)
-
-    sorted_bucket = []
-
-    if chosen_color == 'r':
-        sorted_bucket = sorted(bucket, key = lambda x: x[0])
-    elif chosen_color == 'g':
-        sorted_bucket = sorted(bucket, key = lambda x: x[1])
-    else:
-        sorted_bucket = sorted(bucket, key = lambda x: x[2])
-
+def algorithm(img, depth=4):
+	initial_bucket = [img[x][y] for x in range(img.shape[0]) for y in range(img.shape[1])]
+	return [np.round(np.mean(bucket, axis=0)) for bucket in median_cut(initial_bucket, depth)]
     
