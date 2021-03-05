b90ed6d8de1f67132425cb9bd3b7b11d76599391
Yash Malviya <yashmalviya98@gmail.com>
2021-03-05 12:28:46 +0530

    folder restructure, argparse for main, combined pipeline

--- color_quantize.py
+++ color_quantize.py
@@ -2,18 +2,41 @@ from src.quantize import popularity
 from src.quantize import median_cut
 from src.quantize import floyd_steinberg
 
+from src.quantize import exhaustive_search
+
 from src import util
 
 import cv2 as cv
 import argparse
+import numpy as np
 # import sys
-# import numpy as np
 # np.set_printoptions(threshold=sys.maxsize)
 
-if __name__ == "__main__":
-	img = cv.imread("images/fox.jpg")
-	# util.showimage("img", img)
+median_cut_depth = 5
 
-	# popularity.plot_histogram(img)
-	# floyd_steinberg.dithering_algorithm(img)
-	print(median_cut.algorithm(img))
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="Color Quantization")
+	parser.add_argument("-rf", "--representative-finder", choices=["popularity", "median_cut"], required=True)
+	parser.add_argument("-s", "--search", choices=["exhaustive", "locally_sorted", "kdtree"], required=True)
+	parser.add_argument("-d", "--dithering", action="store_true")
+	parser.add_argument("-o", "--output", type=str, help="Output file to save the quantized image as")
+	parser.add_argument("input_image", type=str, help="Path of image to quantize")
+	args = parser.parse_args()
+	# print(args)
+
+	img = cv.imread(args.input_image)
+
+	representives = popularity.median_cut.algorithm(img) if args.representative_finder=="popularity" else median_cut.algorithm(img, median_cut_depth)
+	quantized_img = None
+	if args.search=="exhaustive":
+		quantized_img = exhaustive_search.quantize(img, representives)
+	elif args.search=="locally_sorted":
+		pass
+	else:
+		pass
+		
+	final_img = floyd_steinberg.dithering_algorithm(img, quantized_img) if args.dithering else quantized_img
+	if args.output:
+		cv.imwrite(args.output)
+	showcase_img = np.concatenate((img, quantized_img), axis=1)
+	util.showimage("side-by-side", showcase_img)
--- src/quantize/floyd_steinberg.py
+++ src/quantize/floyd_steinberg.py
@@ -1,34 +1,18 @@
-import cv2 as cv
 import numpy as np
+from .. import util
 
-def showimage(imglabel, img):
-	cv.imshow(imglabel, img)
-	while True:
-		k = cv.waitKey(0) & 0xFF
-		if k == 27:         # wait for ESC key to exit
-			cv.destroyAllWindows()
-			break
-	cv.destroyAllWindows()
-
-
-def find_closest_palette_color(oldpixel, roundby):
-    r = round(oldpixel[0] * roundby/255) * (255/roundby)
-    g = round(oldpixel[1] * roundby/255) * (255/roundby)
-    b = round(oldpixel[2] * roundby/255) * (255/roundby)
-    r,g,b = int(r), int(g), int(b)
-    return (r,g,b)
-
-def dithering_algorithm(img):
+def dithering_algorithm(img, quantized_img):
+    new_img = quantized_img.copy()
     for y in range(img.shape[1]-1):
         for x in range(1,img.shape[0]-1):
             oldpixel = img[x][y]
-            newpixel = find_closest_palette_color(oldpixel,5)
-            img[x][y] = newpixel
+            newpixel = quantized_img[x][y]
 
             quant_error = oldpixel - newpixel
-            img[x+1][y] = img[x+1][y]+ np.round(quant_error * 7/16)
-            img[x-1][y+1] = img[x-1][y+1] + np.round(quant_error * 3/16)
-            img[x][y+1] = img[x][y+1] + np.round(quant_error * 5/16)
-            img[x+1][y+1] = img[x+1][y+1] + np.round(quant_error * 1/16)
+            new_img[x+1][y] = new_img[x+1][y] + np.round(quant_error * 7/16)
+            new_img[x-1][y+1] = new_img[x-1][y+1] + np.round(quant_error * 3/16)
+            new_img[x][y+1] = new_img[x][y+1] + np.round(quant_error * 5/16)
+            new_img[x+1][y+1] = new_img[x+1][y+1] + np.round(quant_error * 1/16)
 
-    showimage("newimg", img)
+    # util.showimage("new_img", new_img)
+    return new_img
